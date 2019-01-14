67e5fa866a5c34ea4d4f6081c33795384df95778
Arnaud Lemaire <alemaire@norad.fr>
2019-01-14 11:15:00 +0100

    use no-cache when building

--- update-all-images.sh
+++ update-all-images.sh
@@ -1,12 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash
 # simple script to update all images
+set -x
+set -e
 
 PUSH="${PUSH:-false}"
 
 for i in $(ls -d */ | sort); do
   if [ "$i" != "arch/" ]; then
     echo -e "\e[101mBuilding ${i%%/} :\e[0;m"
-    docker build -t "n0rad/${i%%/}" "$i"
+    docker build --no-cache -t "n0rad/${i%%/}" "$i"
     if [ ${PUSH} == true ]; then
       echo -e "\e[101mPushing ${i%%/} :\e[0;m"
       docker push "n0rad/${i%%/}"
