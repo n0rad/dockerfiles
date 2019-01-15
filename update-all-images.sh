6454a39a10ee04497d3ef758345c61cbf11333f4
Arnaud Lemaire <alemaire@norad.fr>
2019-01-15 16:14:29 +0100

    set date tag to docker images

--- update-all-images.sh
+++ update-all-images.sh
@@ -8,10 +8,12 @@ PUSH="${PUSH:-false}"
 for i in $(ls -d */ | sort); do
   if [ "$i" != "arch/" ]; then
     echo -e "\e[101mBuilding ${i%%/} :\e[0;m"
-    docker build --no-cache -t "n0rad/${i%%/}" "$i"
+    current=$(date '+%Y%m%d.%H%M%S')
+    docker build --no-cache -t "n0rad/${i%%/}:latest" -t "n0rad/${i%%/}:${current}" "$i"
     if [ ${PUSH} == true ]; then
       echo -e "\e[101mPushing ${i%%/} :\e[0;m"
-      docker push "n0rad/${i%%/}"
+      docker push "n0rad/${i%%/}:latest"
+      docker push "n0rad/${i%%/}:${current}"
     fi
   fi
 done
