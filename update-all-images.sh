92fb5d96ab1d777f99fdbd9595ec81ac07197363
Arnaud Lemaire <alemaire@norad.fr>
2019-05-06 09:39:50 +0200

    update global build to add hash to tag

--- update-all-images.sh
+++ update-all-images.sh
@@ -7,12 +7,18 @@ PUSH="${PUSH:-false}"
 
 for i in $(find -mindepth 2 -maxdepth 2 -type d | sed -e 's/\.\///' | grep -v ^.git | sort); do
     name=${i##*/}
-    echo -e "\e[101mBuilding ${name} :\e[0;m"
-    current=$(date '+%Y%m%d.%H%M%S')
-    docker build --no-cache -t "n0rad/${name}:latest" -t "n0rad/${name}:${current}" "$i"
-    if [ ${PUSH} == true ]; then
-      echo -e "\e[101mPushing ${name} :\e[0;m"
-      docker push "n0rad/${name}:latest"
-      docker push "n0rad/${name}:${current}"
+    echo -e "\e[101mBuilding $name :\e[0;m"
+    currentDate=$(date '+%Y%m%d.%H%M%S')
+    idFile=$(mktemp)
+    
+    docker build --no-cache --iidfile=$idFile -t "n0rad/$name:latest" "$i"
+
+    if [ $PUSH == true ]; then
+        tag=$currentDate-$(cat $idFile | cut -f2 -d: | cut -c1-8)
+        docker tag $(cat $idFile | cut -f2 -d:) "n0rad/$name:$tag"
+        echo -e "\e[101mPushing $name :\e[0;m"
+        docker push "n0rad/$name:latest"
+        docker push "n0rad/$name:$tag"
+        rm $idFile
     fi
 done
