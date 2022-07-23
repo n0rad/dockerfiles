0203b2813073557e09746b111b3e145ead795971
Arnaud Lemaire <alemaire@norad.fr>
2022-07-23 11:09:18 +0200

    chore(ci): test

--- hack/build-push-from-git-changes.sh
+++ hack/build-push-from-git-changes.sh
@@ -13,13 +13,13 @@ for i in $($DIR/git-changes.sh 2> /dev/null); do
         idFile=$(mktemp)
 
         echo_bright_red "Building $name:"
-        echo docker build --no-cache --iidfile=$idFile -t "n0rad/$name:latest" "$i"
+        docker build --no-cache --iidfile=$idFile -t "n0rad/$name:latest" "$i"
 
         echo_bright_red "Pushing $name:"
         tag="1.$(date '+%Y%m%d').$(date '+%H%M%S')-$(git rev-parse --short HEAD)"
-        echo docker tag $(cat $idFile | cut -f2 -d:) "n0rad/$name:$tag"
-        echo docker push "n0rad/$name:latest"
-        echo docker push "n0rad/$name:$tag"
+        docker tag $(cat $idFile | cut -f2 -d:) "n0rad/$name:$tag"
+        docker push "n0rad/$name:latest"
+        docker push "n0rad/$name:$tag"
     fi
 done
 
