26e48c7c7bb4da3a6b7393df6bb089106c041ae1
Arnaud Lemaire <alemaire@norad.fr>
2022-07-23 11:17:03 +0200

    chore(ci): bump

--- hack/build-push-from-git-changes.sh
+++ hack/build-push-from-git-changes.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/color.sh
 
+PUSH="${PUSH:-false}"
 
 for i in $($DIR/git-changes.sh 2> /dev/null); do
     if [[ "$i" == R:* ]]; then
@@ -15,11 +16,13 @@ for i in $($DIR/git-changes.sh 2> /dev/null); do
         echo_bright_red "Building $name:"
         docker build --no-cache --iidfile=$idFile -t "n0rad/$name:latest" "$i"
 
-        echo_bright_red "Pushing $name:"
-        tag="1.$(date '+%Y%m%d').$(date '+%H%M%S')-$(git rev-parse --short HEAD)"
-        docker tag $(cat $idFile | cut -f2 -d:) "n0rad/$name:$tag"
-        docker push "n0rad/$name:latest"
-        docker push "n0rad/$name:$tag"
+        if [ "$PUSH" = true ]; then
+            echo_bright_red "Pushing $name:"
+            tag="1.$(date '+%Y%m%d').$(date '+%H%M%S')-$(git rev-parse --short HEAD)"
+            docker tag $(cat $idFile | cut -f2 -d:) "n0rad/$name:$tag"
+            docker push "n0rad/$name:latest"
+            docker push "n0rad/$name:$tag"
+        fi
     fi
 done
 
