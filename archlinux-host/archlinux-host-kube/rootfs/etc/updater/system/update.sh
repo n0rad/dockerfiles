24c4c84eb177760866e5e6189fc1f051abdf241f
Arnaud Lemaire <agithub@lmr.fr>
2023-04-08 14:09:18 +0200

    [archlinux-host-kube] keep iptables-nft

--- archlinux-host/archlinux-host-kube/rootfs/etc/updater/system/update.sh
+++ archlinux-host/archlinux-host-kube/rootfs/etc/updater/system/update.sh
@@ -4,9 +4,6 @@ set -x
 readonly SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPT_PATH/lib.sh
 
-# remove iptables if installed so iptables-nft can be installed along kubelet
-pacman --noconfirm -Rdd iptables && pacman --noconfirm -Sy iptables-nft
-
 system_update  " \
     haveged
     open-iscsi nfs-utils rsync
@@ -15,6 +12,3 @@ system_update  " \
     kubelet kubectl kubeadm
     nvidia-container-toolkit nvidia-470xx-dkms
 "
-
-# replace iptables-nft with iptables legacy because of bug
-pacman --noconfirm -Rdd iptables-nft && pacman --noconfirm -S iptables
