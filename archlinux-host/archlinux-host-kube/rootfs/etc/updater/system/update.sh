61ceae2122f98df152729e49465131847e6c5d24
Arnaud Lemaire <agithub@lmr.fr>
2023-04-08 14:14:17 +0200

    [archlinux-host-kube] remove haveged

--- archlinux-host/archlinux-host-kube/rootfs/etc/updater/system/update.sh
+++ archlinux-host/archlinux-host-kube/rootfs/etc/updater/system/update.sh
@@ -5,7 +5,6 @@ readonly SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPT_PATH/lib.sh
 
 system_update  " \
-    haveged
     open-iscsi nfs-utils rsync
     ipset ipvsadm
     cri-o crictl cni-plugins runc
