400b42510b2be75d0bf2be72a86c6d04a9c025f0
Arnaud Lemaire <agithub@lmr.fr>
2023-04-11 01:32:08 +0200

    alpine-asterisk (#45)

---
+++ archlinux-host/archlinux-host-plex-htpc/rootfs/etc/updater/system/lib.sh
@@ -0,0 +1,89 @@
+
+function echo_green {
+  echo -e "\033[0;32m$*\033[0m"
+}
+
+function apply_package_reflector {
+	pacman -Qi reflector &> /dev/null || pacman -S reflector --noconfirm
+	reflector --latest 20 --age 1 --country France,Germany  --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+}
+
+function apply_package_yay {
+	# yay dependencies
+	pacman -Qi wget gcc git grep &> /dev/null || pacman -S wget base-devel git --noconfirm
+
+	# renovate: datasource=github-tags depName=Jguer/yay
+	YAY_VERSION=v11.2.0
+	yay --version | cut -f1 -d- | grep ${YAY_VERSION} > /dev/null || {
+		cd /tmp
+		wget https://github.com/Jguer/yay/releases/download/${YAY_VERSION}/yay_${YAY_VERSION#v*}_x86_64.tar.gz
+		tar xvzf yay_${YAY_VERSION#v*}_x86_64.tar.gz
+		cp yay_${YAY_VERSION#v*}_x86_64/yay /usr/bin
+		cd / 
+		rm -Rf /tmp/yay*
+	}
+	grep "PKGEXT='.pkg.tar'" /etc/makepkg.conf || sed -i -- "s/PKGEXT='.pkg.tar.xz'/PKGEXT='.pkg.tar'/g" /etc/makepkg.conf
+	grep 'yay:' /etc/passwd > /dev/null || echo "yay:x:20002:20002:yay:/var/lib/yay:/usr/bin/sh" >> /etc/passwd
+	grep 'yay:' /etc/group > /dev/null || echo "yay:x:20002:" >> /etc/group
+	grep 'yay:' /etc/shadow > /dev/null || echo "yay:x:20002::::::" >> /etc/shadow
+	mkdir -p /var/lib/yay && chown yay: /var/lib/yay
+	cat > /etc/sudoers.d/yay <<-EOF
+		Cmnd_Alias  PACMAN = /usr/bin/pacman, /usr/bin/yay
+		%yay ALL=(ALL) NOPASSWD: PACMAN
+	EOF
+}
+
+function apply_packages {
+	for i in $1; do
+		if $(pacman -Qeg | cut -f1 -d' ' | grep -q "^$i$") ; then
+			continue # skip installed groups
+		fi
+		pacman -Qi $i &> /dev/null || (echo_green "Installing $i:" && su -c "yay -S $i --noconfirm" yay)
+	done
+}
+
+function apply_default_packages {
+	apply_packages " \
+		syslinux linux intel-ucode systemd-sysvcompat efibootmgr
+		gptfdisk hdparm mdadm dosfstools smartmontools
+		linux-headers dkms
+		openssh
+		pciutils usbutils net-tools bind-tools conntrack-tools
+		ethtool socat iputils iproute2
+		sudo grep curl wget downgrade
+		htop iftop lsof dfc psmisc ncdu tree nmon
+		vim tmux
+	"
+}
+
+function updater_update {
+	mkdir -p /etc/updater/system
+	wget https://raw.githubusercontent.com/n0rad/dockerfiles/master/archlinux-host/archlinux-host/rootfs/etc/updater/system/lib.sh -O /etc/updater/system/lib.sh
+	wget https://raw.githubusercontent.com/n0rad/dockerfiles/master/archlinux-host/$1/rootfs/etc/updater/system/update.sh -O /etc/updater/system/update.sh
+	wget https://raw.githubusercontent.com/n0rad/dockerfiles/master/archlinux-host/$1/rootfs/etc/updater/system/updater-update.sh -O /etc/updater/system/updater-update.sh
+	chmod +x /etc/updater/system/update.sh /etc/updater/system/updater-update.sh
+}
+
+function system_update {
+	echo_green "Updating package list:"
+	pacman -Sy
+
+	echo_green "Updating mirror list:"
+	apply_package_reflector
+
+	echo_green "Preparing arch keys:"
+	pacman-key --init
+	pacman -S --noconfirm archlinux-keyring
+
+	echo_green "Updating installed packages:"
+	pacman -Su --noconfirm
+ 
+	echo_green "Updating yay:"
+	apply_package_yay
+
+	echo_green "Installing default packages:"
+	apply_default_packages
+
+	echo_green "Installing packages:"
+	[ -z "$1" ] || apply_packages "$1"
+}
