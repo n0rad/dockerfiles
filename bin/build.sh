5954d86aff2c2afcacfee51ae68ef2cad0d71ddc
Arnaud Lemaire <alemaire@norad.fr>
2023-05-10 20:49:23 +0200

    up

--- bin/build.sh
+++ bin/build.sh
@@ -12,7 +12,7 @@ name=${path##*/}
 idFile=$(mktemp)
 
 tag="1.$(date -u '+%y%m%d').$(date -u '+%H%M' | awk '{print $0+0}')-H$(git rev-parse --short HEAD)"
-BUILD_ARGS="--platform=linux/amd64,linux/arm/v7,linux/arm64/v8 --no-cache -t n0rad/$name:$tag -t n0rad/$name:latest"
+BUILD_ARGS="--platform=linux/amd64,linux/arm64 --no-cache -t n0rad/$name:$tag -t n0rad/$name:latest"
 if [ "$PUSH" = true ]; then
     echo_bright_red "Building $name:"
     docker buildx build $BUILD_ARGS "$path"
