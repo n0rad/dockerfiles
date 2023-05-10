9550d48a4bcd5bd85bf731e0e71b9942f275a782
Arnaud Lemaire <alemaire@norad.fr>
2023-05-11 01:15:23 +0200

    support build platofrm label

--- bin/build.sh
+++ bin/build.sh
@@ -10,9 +10,14 @@ path=$PWD
 [ -z "$1" ] || path="$(cd "$1" && pwd)"
 name=${path##*/}
 idFile=$(mktemp)
+platform="linux/amd64,linux/arm64"
+platformLabel=$(cat "$path/Dockerfile" | grep -i "^label " | awk -F "[ =]*" '{print $2 " " $3}' | grep -w "platform" | cut -d' ' -f2 | sed -e 's/[" ]*//g' | head -n1 || true)
+if [ "$platformLabel" != "" ]; then
+    platform="$platformLabel"
+fi
 
 tag="1.$(date -u '+%y%m%d').$(date -u '+%H%M' | awk '{print $0+0}')-H$(git rev-parse --short HEAD)"
-BUILD_ARGS="--platform=linux/amd64,linux/arm64 --no-cache -t n0rad/$name:$tag -t n0rad/$name:latest"
+BUILD_ARGS="--platform=$platform --no-cache -t n0rad/$name:$tag -t n0rad/$name:latest"
 if [ "$PUSH" = true ]; then
     echo_bright_red "Building $name:"
     docker buildx build $BUILD_ARGS "$path"
